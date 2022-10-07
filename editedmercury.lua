a81babce7e4bbe41f21f07f02eb265d0c0164cfe
VexusHub <91941885+vrtrz@users.noreply.github.com>
2022-10-07 22:42:37 +0700

    Update editedmercury.lua

--- editedmercury.lua
+++ editedmercury.lua
@@ -50,7 +50,7 @@ local Library = {
 		VapeV4 = {
 			Main = Color3.fromRGB(26, 25, 26),
 			Secondary = Color3.fromRGB(31, 30, 31),
-			Tertiary = Color3.fromRGB(66, 88, 225),
+			Tertiary = Color3.fromRGB(40, 134, 107),
 
 			StrongText = Color3.fromHSV(0, 0, 1),		
 			WeakText = Color3.fromHSV(0, 0, 172/255)
